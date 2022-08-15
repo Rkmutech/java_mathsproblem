0b2618e2ccf6c6340648b619ec641107df574adf
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-15 21:04:42 +0530

    other question

--- question_24/qus_24.java
+++ question_24/qus_24.java
@@ -3,6 +3,7 @@ package question_24;
 public class qus_24 {
     public static void main(String[] args) {
         
+        
     }
 
     
--- question_26/qus_26.java
+++ question_26/qus_26.java
@@ -1,5 +1,8 @@
 package question_26;
 
 public class qus_26 {
+    public static void main(String[] args) {
+        
+    }
     
 }
